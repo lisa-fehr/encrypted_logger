4ebb2f017471a3c3d0624d7c81f33a709bd2c510
Lisa Fehr <lisa@warfehr.com>
2024-03-22 16:07:40 -0400

    Quickly resize a large, high quality image taken with a phone

--- app/Http/Controllers/ConcernController.php
+++ app/Http/Controllers/ConcernController.php
@@ -25,6 +25,7 @@ class ConcernController extends BaseController
 
     function store(Request $request) : \Illuminate\Http\RedirectResponse
     {
+
         $concern = Concern::create(
             $request->except('files')
         );
@@ -33,20 +34,32 @@ class ConcernController extends BaseController
             $files = [];
             foreach($request->file('files') as $key => $file)
             {
-                $fileName = time().rand(1,99).'.'.$file->extension();
-                $img = Crypt::encrypt($file->getContent());
+                $fileName = time().'-'.$file->getClientOriginalName();
+                $uploadedFile = imagecreatefromstring($file->get());
+                
+                
+                $size = getimagesize($file->path());
+
+                $width = (int) ceil($size[0] / 8);
+                $height = (int) ceil($size[1] / 8);
+
+                $thumb = imagecreatetruecolor($width, $height);
+                imagecopyresampled($thumb, $uploadedFile, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+                imagejpeg($thumb, 'storage/app/'.$fileName, 75);
+                imagedestroy($thumb);
+
+                $img = Crypt::encrypt(file_get_contents('storage/app/'.$fileName));
                 Storage::put($fileName, $img);
                 $files[] = $fileName;
             }
 
-
             foreach ($files as $key => $file) {
                 Photo::create([
-                    'user_id' => $request->user()->id,
                     'img' => $file,
                     'concern_id' => $concern->id
                 ]);
             }
+
         }
 
         return back()->with('status', 'Created');
